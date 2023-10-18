d939d2edf6c3cfb96fcc617b9b5865d8c2884f5c
Vinaychelluri <147475977+Vinaychelluri@users.noreply.github.com>
2023-10-18 11:07:13 +0530

    #include<stdio.h> int main() {     int x;     scanf("%d",&x);     if(x>17)     {         printf("YES");
        }
        else if(x<18)
        {
            printf("NO");
        }
    }

---
+++ Casting_the_Vote.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x>17)
+    {
+        printf("YES");
+        
+    }
+    else if(x<18)
+    {
+        printf("NO");
+    }
+}

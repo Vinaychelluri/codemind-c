83fd1c4809dbcb23ec1b40446c3fa798c7577fac
Vinaychelluri <147475977+Vinaychelluri@users.noreply.github.com>
2024-01-05 18:33:06 +0530

    #include<stdio.h> int main() {     int a,b,c=0;     scanf("%d%d",&a,&b);     while(a>0)     {         b=a%10;         c+=b;         a=a/10;
        }
        printf("%d",c);
    }

---
+++ Sum_of_all_digits.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c=0;
+    scanf("%d%d",&a,&b);
+    while(a>0)
+    {
+        b=a%10;
+        c+=b;
+        a=a/10;
+        
+    }
+    printf("%d",c);
+}
